78669e48380484f2f047e39d6fbfbae60cf15c43
Flávio Costa <costaflavio29@gmail.com>
2024-01-25 11:55:38 -0300

    Finishing the project

--- multistep_form_react/src/App.jsx
+++ multistep_form_react/src/App.jsx
@@ -6,17 +6,35 @@ import ReviewForm from './components/ReviewForm';
 import Thanks from './components/Thanks';
 import Steps from './components/Steps';
 
-
 //  Hooks
 import { useForm } from './hooks/useForm';
-
+import { useState } from 'react';
 import './App.css';
 
+const formTemplate = {
+  name: '',
+  email: '',
+  review: '',
+  comment: '',
+};
 
 function App() {
-  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+  const [data, setData] = useState(formTemplate);
 
-  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents);
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return { ...prev, [key]: value };
+    });
+  };
+
+  const formComponents = [
+    <UserForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <Thanks data={data} />,
+  ];
+
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    useForm(formComponents);
 
   return (
     <div className="app">
@@ -25,14 +43,16 @@ function App() {
         <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
       </div>
       <div className="form-container">
-        <Steps currentStep={currentStep}/>
+        <Steps currentStep={currentStep} />
         <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            {!isFirstStep && (<button type="button" onClick={() => changeStep(currentStep - 1)}>
-              <GrFormPrevious />
-              <span>Voltar</span>
-            </button>)}
+            {!isFirstStep && (
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
             {!isLastStep ? (
               <button type="submit">
                 <span>Avançar</span>
